c1c9562300f75915551d47274182fd8da73b4341
S3cBar0n <secberg@gmail.com>
2021-03-27 19:16:26 -0600

    Changed line 47, instead of sending the information to a buffer, it is reading the whole file.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import vt
 import os
 import hashlib
+import pprint
 
 path = ".\Scan"
 token = os.getenv("VT_API")
@@ -10,7 +11,7 @@ token = os.getenv("VT_API")
 if not token:
     token = input("Please enter your Virus Total API Token: ")
 else:
-    exit()
+    pass
 
 
 # Function to perform scanning on files that have not been seen on Virus Total before.
@@ -34,10 +35,8 @@ def vtScan(scan_entry):
 
 # Our main searches the Scan directory for files and checks their hashes against Virus Total.
 def main():
-
-    try:
+    if os.path.isdir(path):
         with os.scandir(path) as directory:
-            print(directory)
             for entry in directory:
                 # It checks to see if a file has a size, if not it skips the file.
                 if os.stat(entry.path).st_size == 0:
@@ -47,11 +46,9 @@ def main():
 
                 with open(entry.path, "rb") as f:
                     client = vt.Client(token)
-                    # The for loop is here for larger files that would not fit into the default buffer
-                    for chunk in iter(lambda: f.read(4096), b""):
-                        md5_hash = hashlib.md5()
-                        md5_hash.update(chunk)
-                        hashed_file = md5_hash.hexdigest()
+                    sha_hash = hashlib.sha256()
+                    sha_hash.update(f.read())
+                    hashed_file = sha_hash.hexdigest()
 
                     # Checks to see if the file is on VT, if not it sends it to the scan function
                     try:
@@ -63,17 +60,21 @@ def main():
                         file = client.get_object(f"/files/{hashed_file}")
 
                     # Printing information VT has collected on this file.
+                    scan_details = file.last_analysis_stats
+
                     print(entry.name)
                     print(hashed_file)
-                    print(file.last_analysis_stats)
+                    pprint.pprint(scan_details)
                     print(f"{file.size} Bytes\n")
+
 
                     # Closing our VT connection and the file last scanned.
                     client.close()
                     f.close()
-    except Exception as e:
-        print(f"Scan Folder does not exist in the same folder as this program: {e}")
-        print("Please create a folder named Scan and re-run the program.")
+    else:
+        print(f"Scan Folder does not exist in the same folder as this program")
+        os.mkdir(".\Scan")
+        print("Folder has been generated.. Please place files in the directory and re-run the program.")
         exit()
 
 
